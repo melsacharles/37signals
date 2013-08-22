994c377677bb302cd2b8e3a4be895e3585c672ed
Melsa <viennajunction@gmail.com>
2013-08-22 14:01:48 +0200

    37signals

---
+++ application.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function(){
+	$(".boxes").mouseenter(function(){
+	$(".container h1").empty();
+	$(".bcpopup ").show();
+	$(".bcpopup ").prepend(); 
+
+
+	//$("boxes").mouseleave(function(){
+		//$("h1").("Making collaboration productive and enjoyable for people every day.");
+	
+
+});
+});
